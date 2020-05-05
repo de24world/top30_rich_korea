681a080c063e6ad2696b39696d8a6fe44000b0e9
Lee, Hosoo <Hosoo.Lee@arithnea.de>
2020-05-05 13:47:10 +0200

    add full image

--- korea10.js
+++ korea10.js
@@ -3,8 +3,8 @@ d3.csv("data.csv", function(error, data) {
 	
 	var svg = d3.select("body")
 					.append("svg")
-					.attr("width",1500)
-					.attr("height",1500);
+					.attr("width",2000)
+					.attr("height",2000);
 
 	data.sort(function(a,b){
 				return d3.descending(+a.value, +b.value);
@@ -24,9 +24,18 @@ d3.csv("data.csv", function(error, data) {
 						return d.value*100;
 					})
 					.attr("height", 20)
-					.attr("fill","#4DAAAB")
+                    .attr("fill","#4DAAAB")
+                    // // toolTip
+                    // .on("mouseover", function() { tooltip.style("display", mull); })
+                    // .on("mouseout",  function() { tooltip.style("display", "none"); })
+                    // .on("mousemove", function(d) {
+                    //     tooltip.style("left", (d3.event.pageX + 10) + "px");
+                    //     tooltip.style("top", (d3.event.pageY - 10) + "px");
+                    //     tooltip.text(d.value); 
+                    // });
+         
 
-            // number from data : 1560, 1407
+            // number from data : 16.8, 7.4 ....
 			var texts = svg.selectAll("text")
 				.data(data)
 				.enter()
@@ -41,17 +50,24 @@ d3.csv("data.csv", function(error, data) {
 					.attr("fill","#777777")
 					.text(function(d) { return d.value;});
             
-            // text in Rect (white)
+            // text in Rect (Lee Kun-hee...)
 			var labels = svg.selectAll(".label")
 				.data(data)
 				.enter()
 				.append("text")
 				.attr("class","label");
 
-				labels.attr("x", 1)
+				labels.attr("x", 5)
 					.attr("y", function(d, i) {
 						return i * 25+15;
 					})
 					.attr("fill","#ffffff")
-					.text(function(d) { return d.Name;});
+                    .text(function(d) { return d.Name;});
+                    
+            // // tooltip append
+            // var tooltip = d3.select("body").append("div")
+            // .attr("class", "toolTip")
+            // .style("display", "none");
+
+
 	});
